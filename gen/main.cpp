278f8b46f6abd439749e4d3afbff4e57a83a6256
Danny <dannyvanswieten@gmail.com>
2018-03-20 13:50:28 +0100

    Initial Commit

---
+++ gen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gen
+//
+//  Created by Danny on 20/03/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
